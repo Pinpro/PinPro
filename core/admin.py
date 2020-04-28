cdd68595d960d031ac7e89fcb6b75656bb5a932e
beaock <beaock@gmail.com>
2020-04-28 23:55:25 +0900

    优化Admin

--- core/admin.py
+++ core/admin.py
@@ -8,16 +8,35 @@ admin.site.site_title = '2zzy后台管理'
 
 
 class PinAdmin(admin.ModelAdmin):
+    # 注册一下批量处理的方法
+    actions = ['make_checked', 'not_passed']  # 请注意这里改成字符串引用了
 
-    list_display = ('id', 'submitter', 'private', 'published', 'image_data', 'tag_list', )
+    # 在amdin里面实现批量操作处理，第一个参数变为self
+    def make_checked(self, request, queryset):
+        queryset.update(check='1')
+    make_checked.short_description = "通过审核"
+
+    def not_passed(self, request, queryset):
+        queryset.update(check='2')
+    not_passed.short_description = "未通过审核"
+
+    # 遍历tag的queryset 拿出每一个的name值来显示在admin里面
+    def tag_name(self, obj):
+        return [a.name for a in obj.tags.all()]
+
+    list_display = ('id', 'submitter', 'check', 'private', 'published', 'image_data', 'tag_name', )
     filter_horizontal = ['likes', ]
+    # search_fields = ['check', 'submitter',]
+    list_editable = ["check", ]
+    list_filter = ["check", "submitter", ]
+
 
 class BoardAdmin(admin.ModelAdmin):
     # 多对多字段显示在adminlist里面的方法
     def pinsID(self, obj):
         return [a.id for a in obj.pins.all()]
 
-    list_display = ('submitter', 'name', 'private', 'published','pinsID')
+    list_display = ('submitter', 'name', 'private', 'published', 'pinsID')
     filter_horizontal = ['pins', ]
 
 
