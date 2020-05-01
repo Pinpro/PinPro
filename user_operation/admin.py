e7e5eede70971437316e1bdaa1ab2ad6bfb4eb0c
beaock <beaock@gmail.com>
2020-05-01 19:25:35 +0900

    后台调整对点赞的管理

--- user_operation/admin.py
+++ user_operation/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 from .models import UserLikes
 
-# Register your models here.
 
+# Register your models here.
 class UserLikesAdmin(admin.ModelAdmin):
-    list_display = ('user', 'pin', 'update_time',)
+    list_display = ('id', 'user', 'pin', 'update_time',)
+    list_filter = ["user", "pin", ]
 
 
 admin.site.register(UserLikes, UserLikesAdmin)
